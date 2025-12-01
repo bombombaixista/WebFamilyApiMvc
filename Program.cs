1f10c2112f9a0e92c55d18e9eda5191046a6e42a
Marco Elias <115870420+bombombaixista@users.noreply.github.com>
2025-12-01 00:23:49 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona serviços ao container
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Força a aplicação a escutar em 0.0.0.0:8080
+app.Urls.Add("http://0.0.0.0:8080");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
