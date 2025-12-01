cb09bc8c7d44b05ef538017ec77cec1b598f62c6
Marco Elias <115870420+bombombaixista@users.noreply.github.com>
2025-12-01 00:27:03 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,21 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona serviços ao container
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configuração do pipeline HTTP
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
-// Força a aplicação a escutar em 0.0.0.0:8080
-app.Urls.Add("http://0.0.0.0:8080");
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
