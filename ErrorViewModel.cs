16e7139ed2e9e91a38718013e85dfb8c7dbbd7d4
Marco Elias <115870420+bombombaixista@users.noreply.github.com>
2025-12-01 00:18:21 -0300

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebFamilyApiMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
